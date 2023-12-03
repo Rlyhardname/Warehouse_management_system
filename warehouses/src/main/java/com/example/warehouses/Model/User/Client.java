cf81662042a3c3fe6b98e24e821ce1f21bdf8167
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-12-03 17:01:25 +0200

    -Seperated current Overlapping Owner/Agent functionality from god controller and service into ClientCon  and ClientService

--- warehouses/src/main/java/com/example/warehouses/Model/User/Client.java
+++ warehouses/src/main/java/com/example/warehouses/Model/User/Client.java
@@ -8,6 +8,8 @@ import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 
+import java.util.UUID;
+
 @Data
 @Getter
 @Setter
@@ -24,22 +26,24 @@ public class Client {
     @Id
     @GeneratedValue(
             strategy = GenerationType.IDENTITY
+            //strategy = GenerationType.AUTO
     )
+    // UUID
     private Long Id;
-    @Email(message ="Email doesn't exists or is not in valid format")
+    @Email(message = "Email doesn't exists or is not in valid format")
     @NotNull
-    @Column(unique=true)
+    @Column(unique = true)
     private String email;
     @NotNull
     @Size(min = 4, max = 20, message = "Passwords needs to be between 4 and 20 characters")
     private String password;
     @NotBlank
     @NotNull
-    @Size(min = 2, max = 30, message = "Name is too short or too long" )
+    @Size(min = 2, max = 30, message = "Name is too short or too long")
     private String firstName;
     @NotBlank
     @NotNull
-    @Size(min = 2, max = 30, message = "Name is too short or too long" )
+    @Size(min = 2, max = 30, message = "Name is too short or too long")
     private String lastName;
     @NotNull
     private String accountType;
--- warehouses/src/main/java/com/example/warehouses/controller/ClientController.java
+++ warehouses/src/main/java/com/example/warehouses/controller/ClientController.java
@@ -1,4 +1,42 @@
 package com.example.warehouses.controller;
 
+import com.example.warehouses.Exception.Client.ClientAlreadyRegisteredException;
+import com.example.warehouses.Model.User.Client;
+import com.example.warehouses.Services.ClientService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/main")
 public class ClientController {
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @SneakyThrows
+    @PostMapping(path = "/register", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
+    )//"application/x-www-form-urlencoded")
+    public ResponseEntity<String> registerClient(@Valid @ModelAttribute Client client,
+                                                 HttpServletResponse response) {
+        if (!clientService.isUsernameTaken(client.getEmail())) {
+            clientService.register(client.getEmail(), client.getPassword(), client.getFirstName(), client.getLastName(), client.getAccountType(), response);
+            return new ResponseEntity<>("User Successfully registered!", HttpStatus.ACCEPTED);
+        } else {
+            throw new ClientAlreadyRegisteredException();
+        }
+    }
+
+    // TODO Login/Configure the Spring Security
 }
