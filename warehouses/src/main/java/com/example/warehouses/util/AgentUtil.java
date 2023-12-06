62c88650111287c16dd235b07901a0501183b909
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-12-06 19:02:40 +0200

    -Broke down 2 methods from AgentService into 5 methods in AgentUtil for more readability and to comply with SOLID. -totalStars, isAgentRated, fetchRatingsList, fetchRentalFormsList, createRentalFormDTO.

--- warehouses/src/main/java/com/example/warehouses/util/AgentUtil.java
+++ warehouses/src/main/java/com/example/warehouses/util/AgentUtil.java
@@ -1,57 +1,45 @@
 package com.example.warehouses.util;
 
-import com.example.warehouses.DTO.AgentAndRentFormDTO;
 import com.example.warehouses.DTO.RentFormDTO;
-import com.example.warehouses.exception.Client.AgentHasNoContractsException;
-import com.example.warehouses.exception.Client.BadPathVariableException;
-import com.example.warehouses.exception.Client.UserNotExististingException;
 import com.example.warehouses.model.AgentRatings;
 import com.example.warehouses.model.user.User;
 import com.example.warehouses.model.warehouse.RentalForm;
 import com.example.warehouses.model.warehouse.Warehouse;
-import com.example.warehouses.repository.UsersRepository;
 import com.example.warehouses.repository.RatingsRepository;
 import com.example.warehouses.repository.RentalFormRepository;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class AgentUtil {
 
-    public static boolean isAgentRated(UsersRepository usersRepository, RatingsRepository ratingsRepository, AgentAndRentFormDTO agentDTO, Long agentId) {
-        User agent = usersRepository.findById(agentId).orElseThrow(() -> new UserNotExististingException());
-        if (agent.getDType().equals("owner")) {
-            throw new BadPathVariableException();
-        }
-
-        agentDTO.setEmail(agent.getEmail());
-        agentDTO.setFirstName(agent.getFirstName());
-        agentDTO.setLastName(agent.getLastName());
-        double ratingsTotal = 0;
-        int numberOfVotes = 0;
-        List<AgentRatings> ratingsOptList = ratingsRepository.findAllByIdAgentID(agentId);
-        if (!ratingsOptList.isEmpty()) {
-            numberOfVotes = ratingsOptList.size();
-            for (AgentRatings rating : ratingsOptList) {
-                ratingsTotal += rating.getStars();
-            }
-
-        } else {
-            return false;
-        }
-
-        agentDTO.setStarsTotal(ratingsTotal);
-        agentDTO.setNumberOfVotes(numberOfVotes);
-        return true;
+    public static boolean isAgentRated(Long agentId, RatingsRepository ratingsRepository) {
+        return ratingsRepository.existsByIdAgentID(agentId);
     }
 
-    public static void gatherFormData(RentalFormRepository rentalFormRepository, AgentAndRentFormDTO agentDTO, Long agentId, LocalDate startDate, LocalDate endDate) {
-        List<RentalForm> rentalForms = rentalFormRepository.findRentFormsByAgentIdAndStartDateEndDate(agentId, startDate, endDate).orElseThrow(() -> new AgentHasNoContractsException());
-        for (RentalForm form : rentalForms) {
-            agentDTO.getRentalForms().add(new RentFormDTO(form.getId(), form.getAgent().getId(), form.getCustomer().getId(), form.getWarehouse().getId(), form.getStartDate(), form.getEndDate(), form.getContractFiatWorth()));
-            System.out.println(form);
+    public static List<AgentRatings> fetchRatingsList(Long agentId, RatingsRepository ratingsRepository) {
+        return ratingsRepository.findAllByIdAgentID(agentId);
+    }
+
+    public static double averageRating(List<AgentRatings> ratings) {
+        double ratingsTotal = 0;
+        for (AgentRatings rating : ratings) {
+            ratingsTotal += rating.getStars();
         }
 
+        return ratingsTotal;
+    }
+
+    public static List<RentalForm> fetchRentalFormsList(RentalFormRepository rentalFormRepository, Long agentId, LocalDate startDate, LocalDate endDate) {
+        List<RentalForm> rentalForms = rentalFormRepository.findRentFormsByAgentIdAndStartDateEndDate(agentId, startDate, endDate);
+        System.out.println(rentalForms.size());
+        // TODO think about if it's good to throw this exception or not..
+        //        if(rentalForms.isEmpty()){
+//            throw new AgentHasNoContractsException();
+//        }
+        return rentalForms;
     }
 
     public static RentalForm createContract(User agent, User user, Warehouse warehouse, LocalDate startDate, LocalDate endDate, double contractFiatWorth, double agentFee) {
@@ -73,4 +61,26 @@ public class AgentUtil {
         return totalRatings / ratingsList.size();
     }
 
+    public static double totalStars(List<AgentRatings> ratings) {
+        double stars = 0;
+        for (AgentRatings curRating : ratings) {
+            stars += curRating.getStars();
+        }
+        return stars;
+    }
+
+    public static List<RentFormDTO> createRentalFormDTO(List<RentalForm> allAgentRentalForms) {
+        List<RentFormDTO> list = new ArrayList<>();
+        for (var item : allAgentRentalForms) {
+            list.add(new RentFormDTO(item.getId(),
+                    item.getAgent().getId(),
+                    item.getCustomer().getId(),
+                    item.getWarehouse().getId(),
+                    item.getStartDate(),
+                    item.getEndDate(),
+                    item.getContractFiatWorth()));
+        }
+
+        return list;
+    }
 }
