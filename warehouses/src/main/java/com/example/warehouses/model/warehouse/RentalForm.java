750b88c58238e0ac2adeffe29793de87cd29d6a5
Ivan Dimitrov <mmo_unstable@abv.bg>
2023-12-06 10:01:50 +0200

    -Add validations in RentalForm class.

--- warehouses/src/main/java/com/example/warehouses/model/warehouse/RentalForm.java
+++ warehouses/src/main/java/com/example/warehouses/model/warehouse/RentalForm.java
@@ -3,7 +3,11 @@ package com.example.warehouses.model.warehouse;
 import com.example.warehouses.model.user.Agent;
 import com.example.warehouses.model.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
@@ -27,9 +31,19 @@ public class RentalForm {
     private User customer;
     @ManyToOne
     private Warehouse warehouse;
+    @Future(message = "Date must be in the future")
+    @NotNull(message = "Date cannot be left blank")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @Future(message = "Date must be in the future")
+    @NotNull(message = "Date cannot be left blank")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+    @NotNull(message = "Fill out fee contract field")
+    @DecimalMin(value = "997.0")
     private double contractFiatWorth;
+    @NotNull(message = "Fill out fee field")
+    @DecimalMin(value = "0.1")
     private double agentFee;
     private Long contractInDays;
 
